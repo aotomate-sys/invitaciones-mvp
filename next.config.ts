d4c93eb52f2016840a016a4c32a0b9a246618eca
aotomate-sys <ao.tomate@gmail.com>
2025-08-20 12:28:06 -0600

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Evita que ESLint falle el build en Vercel (las advertencias seguirán apareciendo en 'npm run lint')
     ignoreDuringBuilds: true,
   },
   experimental: {
